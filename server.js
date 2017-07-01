e6f103935d4d33d4b5a6c8117df7b7b6cc60e2c5
Misshel <jenmish094@gmail.com>
2017-06-30 19:19:01 -0500

    cambias para subir a heroku

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+app.set(‘port’, (process.env.PORT || 5000));
+
 app.use('/static', express.static(path.join(__dirname,'/node_modules')));
 app.use('/static', express.static(path.join(__dirname,'/publics/assets')));
 
 app.get('/', (req, res)=>{
   res.sendFile(__dirname+'/publics/index.html');
 });
-app.listen(8080);
+app.listen(app.get(‘port’), function() {
+  console.log(‘Node app is running on port’, app.get(‘port’));
+});
